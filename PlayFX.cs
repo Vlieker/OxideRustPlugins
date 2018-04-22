2a859968e3e21a3f48e27d85d4e07910f561b06a
Vliek <vliek@raidersheaven.com>
2018-04-22 16:55:14 +0200

    v1.0.2
    Console command and local sound

--- PlayFX.cs
+++ PlayFX.cs
@@ -1,11 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using Oxide.Core;
 using UnityEngine;
 
 namespace Oxide.Plugins
 {
-    [Info("PlayFX", "Vliek", "1.0.1", ResourceId = 2810)]
+    [Info("PlayFX", "Vliek", "1.0.2", ResourceId = 2810)]
     [Description("Play any Rust fx/effect on any specified player.")]
 
     class PlayFX : RustPlugin
@@ -33,37 +32,112 @@ namespace Oxide.Plugins
         [ChatCommand("playfx")]
         void CmdPlayFX(BasePlayer player, string command, string[] args)
         {
-            if (permission.UserHasPermission(player.UserIDString, usagePerm))
+            if (haspermission(player))
             {
-                if (args.Length == 2)
+                playeffect(player, args);
+                return;
+            }
+            Player.Reply(player, GetLang("noPermission", player.UserIDString));
+        }
+
+        [ConsoleCommand("playfx")]
+        void CCmdPlayFX(ConsoleSystem.Arg args)
+        {
+            if(args.Player() == null)
+            {
+                if (args.Args == null)
                 {
-                    ulong effectTarget;
-                    string argTarget = args[0];
-                    string effect = args[1];
-                    try
+                    Puts(GetLang("usageExample", null));
+                    return;
+                }
+                playeffect(null, args.Args);
+            }
+            else
+            {
+                if (args.Args == null)
+                {
+                    Player.Reply(args.Player(), GetLang("usageExample", args.Player().UserIDString));
+                    return;
+                }
+                playeffect(args.Player(), args.Args);
+            }
+        }
+
+        private void playeffect(BasePlayer player, string[] args)
+        {
+
+            if(player == null)
+            {
+                if (args.Length != 2)
+                {
+                    Puts(GetLang("usageExample", null));
+                    return;
+                }
+                string argTarget = args[0];
+                string effect = args[1];
+                if (converttoulong(argTarget))
+                {
+                    ulong effectTarget = Convert.ToUInt64(argTarget);
+                    BasePlayer finaltarget = BasePlayer.FindByID(effectTarget);
+                    if (finaltarget == null)
                     {
-                        effectTarget = Convert.ToUInt64(argTarget);
-                    }
-                    catch (FormatException e)
-                    {
-                        Puts(e.Message);
-                        Player.Reply(player, GetLang("usageExample", player.UserIDString));
+                        Puts(GetLang("noPlayerFound", null));
                         return;
                     }
+                    var finaleffect = new Effect(effect, finaltarget, 0, Vector3.zero, Vector3.forward);
+                    EffectNetwork.Send(finaleffect, finaltarget.net.connection);
+                    Puts("Effect ran on " + finaltarget.displayName);
+                    return;
+                }
+            }
+            else
+            {
+                if (args.Length != 2)
+                {
+                    Player.Reply(player, GetLang("usageExample", player.UserIDString));
+                    return;
+                }
+                string argTarget = args[0];
+                string effect = args[1];
+                if(converttoulong(argTarget))
+                {
+                    ulong effectTarget = Convert.ToUInt64(argTarget);
                     BasePlayer finaltarget = BasePlayer.FindByID(effectTarget);
                     if (finaltarget == null)
                     {
                         Player.Reply(player, GetLang("noPlayerFound", player.UserIDString));
                         return;
                     }
-                    Effect.server.Run(effect, finaltarget.transform.position, Vector3.zero, null, false);
-                    Puts("Ran on " + finaltarget.displayName);
+                    var finaleffect = new Effect(effect, finaltarget, 0, Vector3.zero, Vector3.forward);
+                    EffectNetwork.Send(finaleffect, finaltarget.net.connection);
+                    Player.Reply(player, "Effect ran on " + finaltarget.displayName);
                     return;
                 }
-                Player.Reply(player, GetLang("usageExample", player.UserIDString));
-                return;
             }
-            Player.Reply(player, GetLang("noPermission", player.UserIDString));
+
+        }
+
+        private bool haspermission(BasePlayer player)
+        {
+            if (player == null)
+                return true;
+            if (permission.UserHasPermission(player.UserIDString, usagePerm))
+                return true;
+            return false;
+        }
+
+        private bool converttoulong(string argTarget)
+        {
+            try
+            {
+                ulong converttest = Convert.ToUInt64(argTarget);
+            }
+            catch (FormatException e)
+            {
+                Puts(e.Message);
+                return false;
+            }
+            return true;
         }
     }
 }
